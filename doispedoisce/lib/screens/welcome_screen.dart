206ef8e749202d410bcb250ab44959c29363b5c8
Caio de Araujo <caioa.morais@gmail.com>
2022-11-26 06:14:46 -0300

    Welcome Ok - Começar as mais dificeis

--- doispedoisce/lib/screens/welcome_screen.dart
+++ doispedoisce/lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../util/const_colors.dart';
@@ -30,6 +31,98 @@ class WelcomeScreen extends StatelessWidget {
             ),
             Text('Suas metas e planejamentos na palma da sua mão',
                 style: GoogleFonts.lato(fontSize: 14)),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              width: 260,
+              height: 260,
+              decoration: new BoxDecoration(
+                  color: ConstColors.ccActionAreaM.shade900,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        color: ConstColors.ccActionAreaM.shade900,
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3))
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: new BoxDecoration(
+                          color: ConstColors.ccPersonalTaskM.shade100,
+                          shape: BoxShape.circle),
+                      child: Center(
+                        child: Text('Planeje',
+                            style: GoogleFonts.lato(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: ConstColors.ccBackgroundM.shade900)),
+                      ),
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: new BoxDecoration(
+                              color: ConstColors.ccPersonalTaskM.shade100,
+                              shape: BoxShape.circle),
+                          child: Center(
+                            child: Text('Conquiste',
+                                style: GoogleFonts.lato(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    color: ConstColors.ccBackgroundM.shade900)),
+                          ),
+                        ),
+                        Spacer(),
+                        FaIcon(FontAwesomeIcons.repeat,
+                            color: ConstColors.ccFinancialTaskM.shade900),
+                        Spacer(),
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: new BoxDecoration(
+                              color: ConstColors.ccPersonalTaskM.shade100,
+                              shape: BoxShape.circle),
+                          child: Center(
+                            child: Text('Priorize',
+                                style: GoogleFonts.lato(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: ConstColors.ccBackgroundM.shade900)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: new BoxDecoration(
+                          color: ConstColors.ccPersonalTaskM.shade100,
+                          shape: BoxShape.circle),
+                      child: Center(
+                        child: Text('Conduza',
+                            style: GoogleFonts.lato(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: ConstColors.ccBackgroundM.shade900)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             SizedBox(
               height: 48,
             ),
